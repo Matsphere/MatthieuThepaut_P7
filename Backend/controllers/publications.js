743d0306ecf8a1effc4e1edceb9458f5e072dc29
Matthieu Thepaut <matth.thepautpgmail.com>
2022-01-01 16:47:04 +0100

    corrections routes

--- Backend/controllers/publications.js
+++ Backend/controllers/publications.js
@@ -4,9 +4,25 @@ dotenv.config();
 
 exports.getAllPublications = async (req, res, next) => {
   try {
-    const sql = `SELECT * FROM publications`;
-    await connection.query(sql, (err, result) => {
+    const sql = `SELECT pub.id, pub.author_id, pub.date, pub.text, users.avatar, users.pseudo FROM publications pub 
+    JOIN users
+    ON pub.author_id = users.id`;
+    await connection.query(sql, async (err, result) => {
       if (err) throw err;
+
+      const sql = `SELECT * FROM reactions`
+      await connection.query(sql, (err, res) => {
+        if (err) throw err;
+      result.forEach(pub => {
+        pub.likes = [];
+        pub.dislikes = [];
+       res.forEach(reaction => {
+         if (pub.id == reaction.pub_id && reaction.vote == 1) pub.likes.push(reaction) ;
+         if (pub.id == reaction.pub_id && reaction.vote == -1) pub.likes.push(reaction) ;
+         return;
+       })
+      })
+      });
 
 
       res.status(200).json(result);
@@ -78,23 +94,33 @@ exports.feedback = async (req, res, next) => {
       userId = req.body.userId,
       vote = req.body.vote,
     }
-
+    if (data.vote == 0)  {
+      const sql = `DELETE * FROM reactions WHERE pub_id=? AND voter_id = ?`
+      await connection.query(sql, [req.params.id, data.userId], async (err,result) =>{
+        if (err) throw err;
+        return res.status(201).json({message : 'Vote enregistré!'});
+      
+      })
+    } else {
     const sql = `SELECT EXISTS(SELECT * from reactions WHERE pub_id=? AND voter_id = ?)`;
-    await connection.query(sql, [req.params.id, data.userId], (err,result) =>{
+    await connection.query(sql, [req.params.id, data.userId], async (err,result) =>{
       if (err) throw err;
       if (result) {
         const sql = `UPDATE reactions SET vote = ? WHERE pub_id = ? AND voter_id = ?`
         await connection.query(sql, [data.vote, req.params.id, data.userId ], (err) => {
           if (err) throw err;
         })
+        return res.status(201).json({message : 'Vote enregistré!'});
       } else {
         const sql = `INSERT INTO reactions (voter_id, pub_id, vote) VALUES (?, ?, ?)`
         await connection.query(sql, [data.userId, req.params.id, data.vote ], (err) => {
           if (err) throw err;
         })
+        return res.status(201).json({message : 'Vote enregistré!'});
 
       }
     })
+    }
     
   } catch (err) {
     res.status(400).json({ err });
